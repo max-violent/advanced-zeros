a9dfc18c52657025a55ae9ba52afc37fec0ead64
max-violent <max.violent@gmail.com>
2018-09-25 10:13:51 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var pr={};
+	var i=2;
+	var j=k=s=0;
+	var ch=base;
+	var numb=num=number;
+	while (ch>=i){
+		if(ch%i==0){
+		j=0;
+		while (ch%i==0){
+			j++;
+			ch=ch/i;
+}
+		pr[i]=j;
+}
+		i++;
+}
+	for(k in pr){
+		numb=number;
+		s=0;
+		while(numb>=k){
+			numb=parseInt(numb/k);
+			s=s+numb;
+}
+		if (num>parseInt(s/pr[k])) {
+			num=(parseInt(s/pr[k]))
+
+};
+		
+};
+
+return(num);
+
 }
